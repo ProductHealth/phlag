1c6b02ea15a31a26716ef00060e0d9f4a1d230fc
Niels Draaisma <niels.draaisma@producthealth.com>
2014-10-09 13:20:38 +0100

    Initial commit

---
+++ phlag.go
@@ -0,0 +1,167 @@
+package phlag
+
+import (
+	"github.com/coreos/go-etcd/etcd"
+	"os"
+	"github.com/fatih/structs"
+	"flag"
+	"fmt"
+	"reflect"
+	"strconv"
+	"net/url"
+	"log"
+	"time"
+	"errors"
+)
+
+var flagSet = flag.CommandLine
+var flagSetArgs = os.Args[1:]
+var durationKind = reflect.TypeOf(time.Nanosecond).Kind()
+
+const (
+	phlagTag        = "phlag"
+	descriptionTag  = "description"
+	etcdEndpointVar = "ETCD_ENDPOINT"
+)
+
+type Phlag struct {
+	client           etcdClient
+	etcdPathTemplate string // Etcd location of param, for example '/company.com/config/%v'
+
+}
+
+// Minimal interface definition around etcd client, allows creation of fake in tests
+type etcdClient interface {
+	Get(string, bool, bool) (*etcd.Response, error)
+}
+
+func New(template string) (*Phlag, error) {
+	return NewFromEnvironment(etcdEndpointVar, template)
+}
+
+func NewWithEndpoint(endpoint *url.URL, template string) (*Phlag, error) {
+	var err error
+	switch {
+	case !endpoint.IsAbs() : err = errors.New(fmt.Sprintf("endpoint '%v' is not an absolute url ( http://foo.com:4001 )", endpoint.String()))
+	}
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	log.Printf("Using etcd endpoint : %v", endpoint.String())
+	client := etcd.NewClient([]string{endpoint.String()})
+	client.SetConsistency(etcd.WEAK_CONSISTENCY)
+	return NewWithClient(client, template), nil
+}
+
+func NewFromEnvironment(envName string, template string) (*Phlag, error) {
+	etcdHostEnv := os.Getenv(envName)
+	parsedEtcdUrl, e := url.Parse(etcdHostEnv)
+	var err error
+	switch {
+	case etcdHostEnv == "" : err = errors.New(fmt.Sprintf("environment variable %v not defined", envName))
+	case e != nil : err = errors.New(fmt.Sprintf("%v environment variable does not contain a valid url", envName))
+	}
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	return NewWithEndpoint(parsedEtcdUrl, template)
+}
+
+func NewWithClient(client etcdClient, template string) *Phlag {
+	return &Phlag{client, template }
+}
+
+// Get the named parameter from either the cli or etcd
+func (e *Phlag) Get(name string) *string {
+	if flagGiven(flagSet, name) {
+		valueFromCli := flagSet.Lookup(name)
+		log.Printf("Using command line value %v for param %v", valueFromCli.Value.String(), name)
+		cliValue := valueFromCli.Value.String()
+		return &cliValue
+	} else {
+		// No command line param given, lookup through etcd
+		// log.Printf("Fetching param %v from etcd", name)
+		etcPath := fmt.Sprintf(e.etcdPathTemplate, name)
+		// log.Printf("Using etc path %v", etcPath)
+		valueFromEtcd, err := e.client.Get(etcPath, false, false)
+		if err != nil { // TODO : Sort out '100: Key not found' messages
+			log.Printf(err.Error())
+			return nil
+		}
+		if valueFromEtcd.Node != nil {
+			// log.Printf("Returing node value %v", valueFromEtcd.Node.Value)
+			log.Printf("Using etcd value %v for param %v", valueFromEtcd.Node.Value, name)
+			return &valueFromEtcd.Node.Value
+		}
+		return nil
+	}
+}
+
+func (e *Phlag) Resolve(target interface{}) {
+	s := structs.New(target)
+	for _, field := range s.Fields() {
+		configuredName := field.Tag(phlagTag)
+		description := field.Tag(descriptionTag)
+		switch field.Kind() {
+		case durationKind: flagSet.String(configuredName, field.Value().(time.Duration).String(), description)
+		case reflect.String : flagSet.String(configuredName, field.Value().(string), description)
+		case reflect.Int : flagSet.Int(configuredName, field.Value().(int), description)
+		}
+
+	}
+	flagSet.Parse(flagSetArgs)
+	for _, field := range s.Fields() {
+		configuredName := field.Tag(phlagTag)
+		resolvedValue := e.Get(configuredName)
+		if resolvedValue == nil {
+			log.Printf("Cannot resolve field %v using cli params or etcd", configuredName)
+			continue
+		}
+		// log.Printf("Field %v is of type %v, setting resolved value %v", field.Name(), field.Kind().String(), *resolvedValue)
+		var err error
+		switch {
+		case field.Kind() == durationKind:
+			v := *resolvedValue
+			d, err := time.ParseDuration(v)
+			if err == nil {
+				err = field.Set(d)
+			}
+		case field.Kind() == reflect.String:
+			v := *resolvedValue
+			err = field.Set(v)
+		case field.Kind() == reflect.Int:
+			v, _ := strconv.Atoi(*resolvedValue)
+			err = field.Set(v)
+		case field.Kind() == reflect.Int32:
+			v, _ := strconv.Atoi(*resolvedValue)
+			err = field.Set(v)
+		case field.Kind() == reflect.Int64:
+			v, _ := strconv.Atoi(*resolvedValue)
+			err = field.Set(v)
+		default:
+			log.Printf("Unable to handle reflect.Kind : %v", field.Kind())
+		}
+		if err != nil {
+			log.Printf("Could not set field %v, encoutered error %v", field.Name(), err.Error())
+		}
+		//log.Printf("Field %v now has value %v", field.Name(), field.Value())
+	}
+}
+
+func flagGiven(flagSet *flag.FlagSet, name string) bool {
+	var flags = []string{}
+	flagSet.Visit(func(f *flag.Flag) { flags = append(flags, f.Name)})
+	return stringInSlice(name, flags)
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
---
+++ phlag_test.go
@@ -0,0 +1,177 @@
+package phlag
+
+import (
+	"testing"
+	"flag"
+	"github.com/stretchr/testify/assert"
+	"github.com/coreos/go-etcd/etcd"
+	"os"
+	"fmt"
+	"errors"
+	"time"
+)
+
+type testStruct struct {
+	StringField    string           `phlag:"s"`
+	IntField       int              `phlag:"i"`
+	DurationField  time.Duration    `phlag:"d"`
+}
+
+const (
+	testService  = "testService"
+	PathTemplate = "/test/%v"
+)
+
+type fakeEtcdClient struct {
+	values    map[string]etcd.Response
+}
+
+func newFakeEtcdClient() *fakeEtcdClient {
+	fake := fakeEtcdClient{map[string]etcd.Response{}}
+	return &fake
+}
+
+func StringNodeResponse(value string) etcd.Response {
+	return etcd.Response{
+		Action: "GET",
+		Node: &etcd.Node {
+			Key: "",
+			Value: value,
+			Dir: false,
+			Expiration: nil,
+			TTL: 0,
+			Nodes: etcd.Nodes{},
+			ModifiedIndex: 0,
+			CreatedIndex:0,
+		},
+		PrevNode: nil,
+		EtcdIndex: 0,
+		RaftIndex: 0,
+		RaftTerm: 0,
+	}
+}
+
+func (f *fakeEtcdClient) Set(path string, value string) {
+	f.values[path] = StringNodeResponse(value)
+}
+
+func (f *fakeEtcdClient) Get(path string, _ bool, _ bool) (*etcd.Response, error) {
+	if val, ok := f.values[path]; ok {
+		return &val, nil
+	} else {
+		return nil, errors.New("Not found in fake")
+	}
+}
+
+func TestFlagGivenValidatesGivenFlags(t *testing.T) {
+	fs := flag.NewFlagSet("test", flag.ExitOnError)
+	fs.String("test", "test", "test")
+	fs.String("test2", "test2", "test2")
+	fs.Parse([]string{"--test", "foo"})
+
+	assert.True(t, flagGiven(fs, "test"))
+	assert.False(t, flagGiven(fs, "test2"))
+}
+
+func TestNewClientCreationFailsWhenEnvVarDoesNotExist(t *testing.T) {
+	_, err := NewFromEnvironment("TEST_ENV_1", "")
+	assert.Error(t, err)
+}
+
+func TestNewClientCreationFailsWhenNonAbsoluteUrlGiven(t *testing.T) {
+	EtcdHostEnvVar := "TEST_ENV_2"
+	os.Setenv(EtcdHostEnvVar, "1.2.3.4:4001") // scheme missing
+	_, err := NewFromEnvironment(EtcdHostEnvVar, "/")
+	assert.NotNil(t, err)
+}
+
+func TestNewClientCreationFailsWhenInvalidUrlGiven(t *testing.T) {
+	EtcdHostEnvVar := "TEST_ENV_3"
+	os.Setenv(EtcdHostEnvVar, "thisisnotaurlatall!")
+	_, err := NewFromEnvironment(EtcdHostEnvVar, "")
+	assert.Error(t, err)
+}
+
+func TestNewClientCreationsCompletesWhenGivenAValidUrl(t *testing.T) {
+	EtcdHostEnvVar := "TEST_ENV_4"
+	os.Setenv(EtcdHostEnvVar, "http://localhost:4001")
+	client, err := NewFromEnvironment(EtcdHostEnvVar, "testservice")
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+}
+
+func TestGetResolvesCliParamsFirst(t *testing.T) {
+	// Setup flag
+	flagSet = flag.NewFlagSet("test", flag.ContinueOnError)
+	flagSet.String("param1", "param1", "param1")
+	flagSet.Parse([]string{"--param1", "valuefromflag"})
+
+	// Setup etcd
+	fakeClient := newFakeEtcdClient()
+	fakeClient.Set(fmt.Sprintf(PathTemplate, "param1"), "valuefrometc")
+	ph := NewWithClient(fakeClient, "/params%v")
+	param1 := ph.Get("param1")
+	assert.Equal(t, "valuefromflag", *param1)
+}
+
+func TestGetResolvesEtcdIfCliFails(t *testing.T) {
+	// Setup flag
+	flagSet = flag.NewFlagSet("test", flag.ContinueOnError)
+	flagSet.String("param1", "param1", "param1")
+	flagSet.Parse([]string{"--param1", "valuefromflag"})
+
+	// Setup etcd
+	fakeClient := newFakeEtcdClient()
+	fakeClient.Set(fmt.Sprintf(PathTemplate, "param2"), "valuefrometc")
+	ph := NewWithClient(fakeClient, PathTemplate)
+	param1 := ph.Get("param1")
+	param2 := ph.Get("param2")
+	assert.Equal(t, "valuefromflag", *param1)
+	assert.Equal(t, "valuefrometc", *param2)
+}
+
+func TestGetReturnsNilIfKeyNotAvailable(t *testing.T) {
+	// Setup flag
+	flagSet = flag.NewFlagSet("test1", flag.ContinueOnError)
+	flagSet.Parse([]string{})
+
+	// Setup etcd
+	fakeClient := newFakeEtcdClient()
+	ph := NewWithClient(fakeClient, PathTemplate)
+	param1 := ph.Get("param1")
+	assert.Nil(t, param1)
+}
+
+func TestResolvesPopulatesStringFields(t *testing.T) {
+	// Setup flag
+	flagSet = flag.NewFlagSet("", flag.ContinueOnError)
+	flagSetArgs = []string{"--s", "stringvalue", "--i", "123", "--d", "5m"}
+
+	// Setup etcd
+	fakeClient := newFakeEtcdClient()
+	ph := NewWithClient(fakeClient, PathTemplate)
+
+	s := &testStruct{}
+
+	ph.Resolve(s)
+	assert.Equal(t, s.DurationField, time.Minute*5)
+	assert.Equal(t, s.StringField, "stringvalue")
+	assert.Equal(t, s.IntField, 123)
+}
+
+func TestResolveAllowsMissingFields(t *testing.T) {
+	// Setup flag
+	flagSet = flag.NewFlagSet("", flag.ContinueOnError)
+	flagSetArgs = []string{"--s", "stringvalue"}
+
+	// Setup etcd
+	fakeClient := newFakeEtcdClient()
+	ph := NewWithClient(fakeClient, PathTemplate)
+
+	s := &testStruct{}
+
+	ph.Resolve(s)
+	assert.Equal(t, "stringvalue", s.StringField)
+	assert.Equal(t, 0, s.IntField)
+	assert.Equal(t, 0, s.DurationField)
+}
